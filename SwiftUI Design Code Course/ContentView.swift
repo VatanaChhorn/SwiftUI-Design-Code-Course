b8af3d615ed6a9641dde6b800ca549ae33df6500
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-10-17 17:42:48 +0700

    Initial Commit

---
+++ SwiftUI Design Code Course/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Design Code Course
+//
+//  Created by Vatana Chhorn on 17/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI Design Code Course/SwiftUI_Design_Code_CourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Design_Code_CourseApp.swift
+//  SwiftUI Design Code Course
+//
+//  Created by Vatana Chhorn on 17/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Design_Code_CourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
